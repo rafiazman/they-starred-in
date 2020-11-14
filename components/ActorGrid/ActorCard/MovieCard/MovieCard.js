9e7c22308981f4c99b239468398bd08c081e21a7
Rafi Azman <7603038+rafiazman@users.noreply.github.com>
2020-11-15 00:40:14 +1300

    Use smaller movie posters

--- components/ActorGrid/ActorCard/MovieCard/MovieCard.js
+++ components/ActorGrid/ActorCard/MovieCard/MovieCard.js
@@ -12,14 +12,14 @@ class MovieCard extends React.Component {
         const { movie, className } = this.props
         const { poster_path, media_type, release_date, first_air_date } = movie
 
-        const posterImage = `https://image.tmdb.org/t/p/w500${poster_path}`
+        const posterImage = `https://image.tmdb.org/t/p/w300${poster_path}`
 
         const name = media_type === 'tv' ? movie.name : movie.title
         const year = media_type === 'tv' ? first_air_date.substring(0,4) : release_date.substring(0, 4)
 
         return (
             <div className={className}>
-                { poster_path ? <Image src={posterImage} width={500} height={750}></Image> : <Image src="/missing-poster.png" width={500} height={750}></Image> }
+                { poster_path ? <Image src={posterImage} width={300} height={450}></Image> : <Image src="/missing-poster.png" width={300} height={450}></Image> }
 
                 <Heading size="md">{name} {`(${year})`}</Heading>
             </div>
