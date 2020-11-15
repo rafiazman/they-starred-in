43e1444c5f64fff3b96feb2d75306249852f8ecc
Rafi Azman <7603038+rafiazman@users.noreply.github.com>
2020-11-15 16:53:02 +1300

    Add link to detail page for each movie

--- components/ActorGrid/ActorCard/MovieCard/MovieCard.js
+++ components/ActorGrid/ActorCard/MovieCard/MovieCard.js
@@ -1,35 +1,37 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
-import { Heading } from "@chakra-ui/react"
+import {Heading} from "@chakra-ui/react"
 
 class MovieCard extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const {movie, className} = this.props
+    const {poster_path, release_date, original_title} = movie
+
+    const posterImage = `https://image.tmdb.org/t/p/w300${poster_path}`
+    const name = original_title
+    const year = release_date ? `(${release_date.substring(0, 4)})` : ''
+
+    const headingCss = {
+      overflow: 'hidden',
+      whiteSpace: 'nowrap',
+      textOverflow: 'ellipsis',
     }
 
-    render() {
-        const { movie, className } = this.props
-        const { poster_path, release_date, original_title } = movie
+    return (
+      <a href={`https://www.themoviedb.org/movie/${movie.id}`} className={className} style={{width: '300px'}}>
+        {poster_path ? <Image src={posterImage} width={300} height={450}></Image> :
+          <Image src="/missing-poster.png" width={300} height={450}></Image>}
 
-        const posterImage = `https://image.tmdb.org/t/p/w300${poster_path}`
-        const name = original_title
-        const year = release_date ? `(${release_date.substring(0, 4)})` : ''
-
-        const headingCss = {
-            overflow: 'hidden',
-            whiteSpace: 'nowrap',
-            textOverflow: 'ellipsis',
-        }
-
-        return (
-            <div className={className} style={{ width: '300px' }}>
-                { poster_path ? <Image src={posterImage} width={300} height={450}></Image> : <Image src="/missing-poster.png" width={300} height={450}></Image> }
-
-                <Heading size="md" css={headingCss}>{name} {year}</Heading>
-            </div>
-        )
-    }
+        <Heading size="md" css={headingCss}>{name} {year}</Heading>
+      </a>
+    )
+  }
 }
 
 export default MovieCard;
