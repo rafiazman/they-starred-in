dae2fcc797f5f7a67dfc09e4067a87b2f510c71f
Rafi Azman <7603038+rafiazman@users.noreply.github.com>
2020-11-15 20:00:34 +1300

    Sort movies according to release year before sending to frontend

--- pages/api/actor/[actor].js
+++ pages/api/actor/[actor].js
@@ -1,15 +1,26 @@
 import axios from 'axios'
-import { API_HOSTNAME_PEOPLE } from "../../../constants";
+import {API_HOSTNAME_PEOPLE} from "../../../constants";
 
 async function handler(req, res) {
-    const {
-        query: { actor },
-    } = req
+  const {
+    query: {actor},
+  } = req
 
-    const { API_KEY } = process.env
-    const results = await axios.get(`${API_HOSTNAME_PEOPLE}/${actor}/movie_credits?api_key=${API_KEY}`)
+  const {API_KEY} = process.env
+  const url = `${API_HOSTNAME_PEOPLE}/${actor}/movie_credits?api_key=${API_KEY}`
+  const {data} = await axios.get(url)
 
-    res.json(results.data.cast)
+  const sortedData = data.cast.sort( (movieA, movieB) => {
+    const releaseDateA = movieA.release_date
+    const releaseDateB = movieB.release_date
+
+    const releaseYearA = releaseDateA ? Number(releaseDateA.substring(0, 4)) : -1
+    const releaseYearB = releaseDateB ? Number(releaseDateB.substring(0, 4)) : -1
+
+    return releaseYearB - releaseYearA
+  })
+
+  res.json(sortedData)
 }
 
 export default handler
