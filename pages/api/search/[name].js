c1430573608a32068aad7c35cfa28a7847c972e9
Rafi Azman <7603038+rafiazman@users.noreply.github.com>
2020-11-15 19:48:46 +1300

    Refactor API route for searching actor

--- pages/api/search/[name].js
+++ pages/api/search/[name].js
@@ -1,15 +1,17 @@
 import axios from 'axios'
-import { API_HOSTNAME_SEARCH_PERSON } from "../../../constants";
+import {API_HOSTNAME_SEARCH_PERSON} from "../../../constants";
 
 async function handler(req, res) {
-    const {
-        query: {name},
-    } = req
+  const {
+    query: {name},
+  } = req
 
-    const { API_KEY } = process.env
-    const results = await axios.get(`${API_HOSTNAME_SEARCH_PERSON}?api_key=${API_KEY}&query=${encodeURIComponent(name)}`)
+  const {API_KEY} = process.env
+  const url = `${API_HOSTNAME_SEARCH_PERSON}?api_key=${API_KEY}&query=${encodeURIComponent(name)}`
 
-    res.json(results.data)
+  const {data} = await axios.get(url)
+
+  res.json(data)
 }
 
 export default handler
